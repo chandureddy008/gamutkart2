fa3e10bcfc2cc9d282ad460305a84a1702ee2004
chandureddy008 <chanduhpreddy@gmail.com>
2023-10-24 23:56:57 +0530

    changes made

---
+++ src/main/java/com/gamutkart/app.java
@@ -0,0 +1,14 @@
+package com.gamukart;
+
+public class App {
+    public static void main(String[] args) {
+        int i; // Removed the colon ":" and declared i as an integer
+        int j; // Removed the colon ":" and declared j as an integer
+
+        for (i = 0; i <= 100; i++) { // Changed the semicolon to a colon
+            i += 5; // Corrected the addition
+            i += 10; // Corrected the addition
+        }
+    }
+}
+
